7beee380be77fb5d950392fe36a7ed53cbade8e8
liitx <ab@liitx.com>
2023-08-14 17:11:01 -0500

    refactored the home_page.dart so per state it will run the helper_funciton instead of all the UI being on one screen

--- lib/ui/pages/home/helper_functions.dart
+++ lib/ui/pages/home/helper_functions.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/ui/pages/home/cubits/post/post_cubit.dart';
 
-buildInitialInput() {}
+buildInitialInput(
+    {required BuildContext context,
+    required PostInitial state,
+    required PostCubit postController}) {
+  return Center(
+    child: Column(
+      children: [
+        Text(state.initialMessage.toString()),
+        ElevatedButton(
+            onPressed: () {
+              postController.fetchPost();
+            },
+            child: const Text('Refresh'))
+      ],
+    ),
+  );
+}
 
 buildLoading() {
   return const Center(
@@ -8,8 +25,63 @@ buildLoading() {
   );
 }
 
-buildLoaded(
-  post,
-) {}
+buildLoaded({
+  required BuildContext context,
+  required PostLoaded state,
+  posts,
+  required PostCubit postController,
+}) {
+  return SingleChildScrollView(
+    child: Column(children: [
+      ...posts.map((e) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+                height: MediaQuery.sizeOf(context).height * .38,
+                child: Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text('UserID: ${e.userId.toString()}'),
+                      const Divider(),
+                      Text('ID: ${e.id.toString()}'),
+                      const Divider(),
+                      Text('TITLE: ${e.title.toString()}'),
+                      const Divider(),
+                      Text('BODY: ${e.body.toString()}'),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Favorite',
+                                  style: TextStyle(color: Colors.orange),
+                                )),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  postController.deletePost(e.id);
+                                },
+                                child: const Text(
+                                  'Delete',
+                                  style: TextStyle(color: Colors.redAccent),
+                                ))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ))),
+          )),
+    ]),
+  );
+}
 
-buildError(state) {}
+buildError({required state}) {
+  return Text(state.errorMessage.toString());
+}
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_app/ui/pages/home/cubits/post/post_cubit.dart';
+import 'package:test_app/ui/pages/home/helper_functions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,97 +21,24 @@ class HomePage extends StatelessWidget {
             child: BlocBuilder<PostCubit, PostState>(
               builder: (context, state) {
                 if (state is PostInitial) {
-                  return Center(
-                    child: Column(
-                      children: [
-                        Text(state.initialMessage.toString()),
-                        ElevatedButton(
-                            onPressed: () {
-                              postContoller.fetchPost();
-                            },
-                            child: const Text('Refresh'))
-                      ],
-                    ),
-                  );
+                  return buildInitialInput(
+                      context: context,
+                      state: state,
+                      postController: postContoller);
                 } else if (state is PostLoading) {
-                  return const Center(child: CircularProgressIndicator());
+                  return buildLoading();
                 } else if (state is PostLoaded) {
-                  final post = (state).post;
-                  return SingleChildScrollView(
-                    child: Column(children: [
-                      ...post.map((e) => Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: SizedBox(
-                                height: MediaQuery.sizeOf(context).height * .35,
-                                child: Card(
-                                    child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    children: [
-                                      // Text(
-                                      //     ': ${e.userId.toString()}'),
-                                      // Divider(),
-                                      Text('ID: ${e.id.toString()}'),
-                                      const Divider(),
-                                      Text('TITLE: ${e.title.toString()}'),
-                                      const Divider(),
-                                      Text('BODY: ${e.body.toString()}'),
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 10, bottom: 10),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            ElevatedButton(
-                                                onPressed: () {},
-                                                child: const Text(
-                                                  'Favorite',
-                                                  style: TextStyle(
-                                                      color: Colors.orange),
-                                                )),
-                                            const SizedBox(
-                                              width: 10,
-                                            ),
-                                            ElevatedButton(
-                                                onPressed: () {
-                                                  postContoller
-                                                      .deletePost(e.id);
-                                                },
-                                                child: const Text(
-                                                  'Delete',
-                                                  style: TextStyle(
-                                                      color: Colors.redAccent),
-                                                ))
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ))),
-                          )),
-                    ]),
-                  );
+                  final posts = state.post;
+                  return buildLoaded(
+                      context: context,
+                      state: state,
+                      postController: postContoller,
+                      posts: posts);
                 } else if (state is PostError) {
-                  return Text(state.errorMessage.toString());
+                  return buildError(state: state);
                 } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
+                  return buildError(state: state);
                 }
-
-                // if (state is PostInitial) {
-                //   return buildInitialInput();
-                // } else if (state is PostLoading) {
-                //   return buildLoading();
-                // } else if (state is PostLoaded) {
-                //   final post = (state as PostLoaded).post;
-                //   return buildLoaded(post, context);
-                // } else if (state is PostError) {
-                //   return buildError(state);
-                // } else {
-                //   return buildInitialInput();
-                // }
               },
             ),
           ),
